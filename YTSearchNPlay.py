c58d5754d210247f0aeb12d32b50b0b670f5e1e2
Pratik Sharma <55416418+pratiksharma8@users.noreply.github.com>
2020-03-19 11:41:56 -0700

    Add files via upload

---
+++ YTSearchNPlay.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+
+# print("This program only plays the first suggestion on YouTube \n")
+# key = input("What do you want to search?")
+key = "SURAKSHYA GHIMIRE VLOG SAN FRANCISCO TO LOS ANGELES"
+
+driver = webdriver.Chrome('/Users/pratik/Desktop/Fun Automation/chromedriver')
+driver.get('https://www.youtube.com')
+
+searchbox = driver.find_element_by_xpath('//*[@id="search"]')
+searchbox.send_keys(key)
+
+searchbutton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+searchbutton.click()
+
+time.sleep(2)
+
+play = driver.find_element_by_xpath('//*[@id="video-title"]/yt-formatted-string')
+play.click()
